e30606c5f490ff0f5a6cdafce07afd28195a4f66
Vishal <vishal@Vishals-MacBook-Pro.local>
2025-01-02 12:42:05 +0530

    code clean

--- src/componets/molecules/ExerciseListView.tsx
+++ src/componets/molecules/ExerciseListView.tsx
@@ -1,66 +1,35 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {colors} from '../../styles/colors';
 
 interface IProps {
   data: any;
 }
 
 const ExerciseListView = (props: IProps) => {
-  console.log('propsss-->>>');
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        width: '100%',
-      }}>
+    <View style={styles.container}>
       <FastImage
-        style={{width: 80, height: 80, borderRadius: 10, margin: 16}}
+        style={styles.iconStyle}
         source={{
-          uri: 'https://drive.usercontent.google.com/download?id=1BmI1kmWC5oRsKgw-XMNbuY3ItyyeaHh0&export=download',
+          uri: props.data.image,
           priority: FastImage.priority.high,
         }}
         resizeMode={FastImage.resizeMode.cover}
       />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          width: 280,
-          alignItems: 'center',
-        }}>
+      <View style={styles.innerContainer}>
         <View>
-          <Text
-            style={{
-              fontSize: 28,
-              fontFamily: 'Poppins-Bold',
-              color: '#fff',
-            }}>
-            {props.data.skillName}
-          </Text>
-          <View
-            style={{
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 16,
-            }}>
-            <Text
-              style={{
-                fontSize: 16,
-                fontFamily: 'Poppins-Bold',
-                color: '#000',
-              }}>
+          <Text style={styles.skillText}>{props.data.skillName}</Text>
+          <View style={styles.exercise}>
+            <Text style={styles.noOfExercise}>
               {props.data.noOfItems} exercises
             </Text>
           </View>
         </View>
         <View>
           <Image
-            style={{height: 32, width: 32, tintColor: 'gray'}}
+            style={styles.arrowStyle}
             source={require('../../assets/images/right.png')}
           />
         </View>
@@ -70,3 +39,48 @@ const ExerciseListView = (props: IProps) => {
 };
 
 export default ExerciseListView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 280,
+    alignItems: 'center',
+  },
+  skillText: {
+    fontSize: 24,
+    fontFamily: 'Poppins-Bold',
+    color: colors.white,
+    marginBottom: 4,
+  },
+  exercise: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    padding: 4,
+  },
+  noOfExercise: {
+    fontSize: 12,
+    fontFamily: 'Poppins-Bold',
+    color: '#000',
+  },
+  arrowStyle: {
+    height: 32,
+    width: 32,
+    tintColor: 'gray',
+  },
+  iconStyle: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    margin: 16,
+  },
+});
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,34 +1,62 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, ActivityIndicator, FlatList} from 'react-native';
+import {View, Text, Image, FlatList, SafeAreaView} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import FootballLoader from '../../componets/atoms/FootballLoader';
 import ExerciseListView from '../../componets/molecules/ExerciseListView';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import styles from './styles';
 
 const HomeScreen = () => {
-  const [skillData, setSkillData] = useState(null);
+  const [skillData, setSkillData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<any>([]);
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('skills')
+      .onSnapshot(querySnapshot => {
+        const user: any[] = [];
+
+        querySnapshot.forEach(documentSnapshot => {
+          user.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setUsers(user);
+        setLoading(false);
+      });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  console.log('userr-->>>>', users);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const snapshot = await firestore()
           .collection('skills')
-          .doc('1hDFOBtH7ieob0KTK4w8')
-          .get();
+          .onSnapshot(querySnapshot => {
+            const user: any[] = [];
 
-        if (snapshot.exists) {
-          setSkillData([snapshot.data()]);
-        } else {
-          console.log('No such document!');
-        }
+            querySnapshot.forEach(documentSnapshot => {
+              user.push({
+                ...documentSnapshot.data(),
+                key: documentSnapshot.id,
+              });
+            });
+
+            setSkillData(user);
+            setLoading(false);
+          });
       } catch (error) {
         console.error('Error fetching Firestore data: ', error);
       } finally {
         setTimeout(() => {
           setLoading(false);
-        }, 2000);
+        }, 3000);
       }
     };
     fetchData();
@@ -39,7 +67,17 @@ const HomeScreen = () => {
     return <ExerciseListView data={item} />;
   };
 
-  console.log(skillData);
+  const errorView = () => {
+    return (
+      <View style={styles.errorView}>
+        <Image
+          style={styles.oopsImage}
+          source={require('../../assets/images/oops.png')}
+        />
+        <Text style={styles.noDataText}>Oops, Something went wrong...</Text>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -48,10 +86,7 @@ const HomeScreen = () => {
       ) : skillData ? (
         <FlatList data={skillData} renderItem={renderItem} />
       ) : (
-        <View style={styles.errorView}>
-          <Image style={styles.oopsImage} source={require('../../assets/images/oops.png')}/>
-        <Text style={styles.noDataText}>Oops, Something went wrong...</Text>
-        </View>
+        errorView()
       )}
     </SafeAreaView>
   );
